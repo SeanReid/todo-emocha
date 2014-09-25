6f987956d3f9043bfb3930384f34f021e0f478f1
Sean Reid <sreid757@gmail.com>
2014-09-25 11:18:01 -0400

    Seed data added, Minimum requirements met

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.delete_all
+
+Task.create! title: "Hire Sean Reid"
+Task.create! title: "Give Sean a Raise"
+Task.create! title: "Give Sean a Promotion"
