a1e5acfc0bbd5a378d4e67f84ae49c4f5e9eca36
Sean Reid <sreid757@gmail.com>
2014-09-25 13:24:15 -0400

    changed seed data for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Task.delete_all
 
-Task.create! title: "Hire Sean Reid"
-Task.create! title: "Give Sean a Raise"
 Task.create! title: "Give Sean a Promotion"
+Task.create! title: "Give Sean a Raise"
+Task.create! title: "Hire Sean Reid"
+Task.create! title: "Be Amazed by ZERO refreshes!"
+Task.create! title: "Check Out Awesome Modals"
